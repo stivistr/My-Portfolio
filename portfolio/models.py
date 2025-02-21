abb193112c1f63c7a80e6691bbd841b62f8eb476
stivist <stivan_strahilov@abv.bg>
2025-02-21 13:05:22 +0200

    Added Project model and configured environment variables

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
