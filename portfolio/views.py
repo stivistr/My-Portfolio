0d26bd059b84d43bc461770c2050ccbbff0657af
stivist <stivan_strahilov@abv.bg>
2025-02-22 11:58:47 +0200

    add new view and contact form

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Project
+from .forms import ContactForm
 
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects':projects})
 
 
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # For now, we'll just print the data to the console.
+            print(form.cleaned_data)
+    else:
+        form = ContactForm()
+
+    return render(request, 'portfolio/contact.html', {'form': form})
