4ea6485ae4d093f6d13c68c1dd6d902fe5ee93f3
stivist <stivan_strahilov@abv.bg>
2025-02-22 13:04:48 +0200

    fix to contant via mail

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +130,13 @@ STATICFILES_DIRS = [BASE_DIR / 'portfolio/static']
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Project
 from .forms import ContactForm
+from django.conf import settings
+from django.core.mail import send_mail
 
 def home(request):
     projects = Project.objects.all()
@@ -11,8 +13,19 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            # For now, we'll just print the data to the console.
-            print(form.cleaned_data)
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            # Construct email message
+            subject = f'New Contact Form Submission from {name}'
+            full_message = f'Name: {name}\nEmail: {email}\n\nMessage:\n{message}'
+
+            # Send email
+            send_mail(subject, full_message, settings.DEFAULT_FROM_EMAIL, ['stivistr@gmail.com'])
+
+            return render(request, 'portfolio/contact.html', {'form': form, 'success': True})
+
     else:
         form = ContactForm()
 
