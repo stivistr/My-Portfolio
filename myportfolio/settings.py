11778723eccad51102b3dfe318b02905c55ce46d
stivist <stivan_strahilov@abv.bg>
2025-02-24 18:23:28 +0200

    Updated settings file

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-hmfc01gtlcgtbjc6l8b&l-@!*0$#&1l2=%pkv$ponk0=o!3_&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stivistrportfolio.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'portfolio/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
