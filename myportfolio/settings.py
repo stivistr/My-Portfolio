eacfd6031d407f53d1214705aa60dd896834652f
stivist <stivan_strahilov@abv.bg>
2025-03-04 17:51:56 +0200

    changes for a deployment

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -81,7 +81,11 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
